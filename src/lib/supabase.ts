04acf7383ec5035830a18bb97daaae6c3743038e
truongthedien123456-sketch <truongthedien123456@gmail.com>
2026-08-26 15:10:08 +0700

    Connect to new Supabase project via env vars

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://quvqnmihlzelepgkrwco.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1dnFubWlobHplbGVwZ2tyd2NvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxNTE2ODUsImV4cCI6MjEwMTcyNzY4NX0.6-VIUelzqzGsQIft8mMY83FerJjW_ScB6erCbgitK70';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
